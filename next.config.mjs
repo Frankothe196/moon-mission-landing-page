4dea5953f8691373e97cbd79d262f3dcd5e4c10f
Franklin Barto <kipkalisbartoo@gmail.com>
2024-02-11 22:25:39 +0300

    Nojekyll and workflow update

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
-    distDir: 'out'
+    basePath: "/moon-mission-landing-page",
+    images: {
+      unoptimized: true,
+    },
 };
 
 export default nextConfig;
